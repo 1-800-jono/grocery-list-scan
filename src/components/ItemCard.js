fb2b3452a6a7ba34deba248659bf047e4bea4cc0
Jonathan Perez <jonathan.phz@gmail.com>
2018-07-04 17:46:21 -0300

    Customize ItemCard

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -18,26 +18,41 @@ export default class ItemCard extends React.Component {
     //const img = require(this.props.item.image);
     
     return (
-      <View style={styles.container}>
+      <View style={styles.itemContainer}>
         <Image source={this.props.item.image} style={styles.image} />
-        <Text style={styles.text}>{this.props.item.name}</Text>
+        <View>
+          <Text style={styles.text}>{this.props.item.name}</Text>
+          <View>
+            <Text style={styles.text}>x2</Text>
+          </View>
+        </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
     //justifyContent: 'space-between',
     marginBottom: 10,
-    width: '30%'
+    height: 83,
+    width: '100%',
+    backgroundColor: '#222',
+    padding: 4,
+    borderRadius: 5
   },
   text: {
-    color: '#fff'
+    color: '#fff',
+    fontFamily: 'System',
+    marginTop: 3
   },
   image: {
     marginBottom: 8,
-    width: '100%', 
-    height: 100
+    width: 75, 
+    height: 75,
+    marginRight: 8,
+    borderRadius: 5,
   }
 });
