2fce33e6805a97352c4f708e9e102d052586e2b6
Jonathan Perez <jonathan.phz@gmail.com>
2018-07-17 18:59:06 -0300

    add details

--- src/components/routing/router.js
+++ src/components/routing/router.js
@@ -1,15 +1,39 @@
 import React from 'react';
-import {TabNavigator} from 'react-navigation';
+import {createBottomTabNavigator, createStackNavigator} from 'react-navigation';
 import {Icon} from 'react-native-elements';
 
-import Intro from '../../../Intro';
+import List from '../List';
+import ItemDetails from '../ItemDetails';
 import BarcodeScanner from '../BarCodeScanner';
 
-export const Tabs = TabNavigator({
+export const ItemStack = createStackNavigator({
   List: {
-    screen: Intro,
+    screen: List,
+    navigationOptions: {
+      title: 'List'
+    },
+  },
+  ItemDetails: {
+    screen: ItemDetails,
+    navigationOptions:  ({navigation}) => ({
+        title: `${navigation.state.params.name}`
+      })
+  }
+});
+
+export const Tabs = createBottomTabNavigator({
+  List: {
+    screen: ItemStack,
+    navigationOptions: {
+      tabBarLabel: 'List',
+      tabBarIcon: ({tintColor}) => <Icon name="list" size={35} color={tintColor}/>,
+    }
   },
   BarcodeScanner: {
     screen: BarcodeScanner,
+    navigationOptions: {
+      tabBarLabel: 'Barcode',
+      tabBarIcon: ({tintColor}) => <Icon name="barcode-scan" type="material-community" size={35} color={tintColor}/>,
+    }
   }
 });
