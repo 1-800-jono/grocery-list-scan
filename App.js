8bc3d0e46558a96d3205866ffaca083123277582
Jonathan Perez <jonathan.phz@gmail.com>
2018-07-18 19:37:50 -0300

    Add root stacknavigator

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { View, StatusBar } from 'react-native';
+//import base from './src/base';
 
 import sampleItems from './src/temp-data/sample-fishes'; 
 
-import {Tabs} from './src/components/routing/router';
+import {Root} from './src/components/routing/router';
 
 
 class App extends Component {
@@ -20,7 +21,7 @@ class App extends Component {
     return (
       <View style={{ flex: 1 }}>
         <StatusBar backgroundColor="blue" barStyle="light-content"/>
-        <Tabs screenProps={{items: this.state.items}}/>
+        <Root screenProps={{items: this.state.items}}/>
      </View>
     );
   }
