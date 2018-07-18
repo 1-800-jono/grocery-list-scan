f04a3de3b7a3e3ff2c4bf272846f17d57a0cb9f7
Jonathan Perez <jonathan.phz@gmail.com>
2018-07-18 12:07:31 -0300

    remove unused variables

--- src/screens/List.js
+++ src/screens/List.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity} from 'react-native';
+import { StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
 import ItemCard from '../components/ItemCard';
 
 
