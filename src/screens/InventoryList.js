94154063b24f1376584081fca1134dd1f29e5e4f
Jonathan Perez <jonathan.phz@gmail.com>
2018-07-19 15:59:51 -0300

    Pass addBuyList method

---
+++ src/screens/InventoryList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
+import ItemCard from '../components/ItemCard';
+
+
+export default class InventoryList extends React.Component {
+
+  onShowDetails = (item) => {
+    //alert(item.name);
+    this.props.navigation.navigate('ItemDetails', {...item})
+  };
+  
+  render() {
+    return (
+      <ScrollView style={styles.container}>
+        {
+          Object.keys(this.props.screenProps.inventory).map((key) => (
+            <TouchableOpacity onPress={() => this.onShowDetails(this.props.screenProps.inventory[key])} key={key}>
+             <ItemCard               
+                item={this.props.screenProps.inventory[key]} 
+                index={key}    
+                addToBuyList={this.props.screenProps.addToBuyList}         
+              />
+           </TouchableOpacity>
+        ))}
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    //justifyContent: 'space-between',
+    //flexWrap: 'wrap',
+    backgroundColor: '#111',
+    padding: 20
+  },
+  text: {
+    color: '#fff'
+  },
+  button: {
+    backgroundColor: 'tomato',
+    width: '100%'
+  },
+  scroll: {
+    height: '100%'
+  }
+});
