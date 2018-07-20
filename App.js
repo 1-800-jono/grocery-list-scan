add439ef8a11a5ebd02faac1799dee97d4804e88
Jonathan Perez <jonathan.phz@gmail.com>
2018-07-20 18:40:21 -0300

    add AsyncStorage methods

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 console.ignoredYellowBox = ['Remote debugger']; //Temp fix for annoying warning
 import React, { Component } from 'react';
-import { View, StatusBar } from 'react-native';
+import { View, StatusBar, AsyncStorage } from 'react-native';
 //import base from './src/base';
 
 import sampleItems from './src/temp-data/sample-fishes'; 
@@ -12,23 +12,59 @@ class App extends Component {
 
   state = {
     inventory: {},
-    buyList: {}
+    buyList: {},
+    userID: 'jonathan'
   }
 
-  // componentWillMount() {
-  //   this.setState({ inventory: sampleItems });
-  // }
+   componentDidMount() {
+     this.getLocalStorage();
+  }
+
+  saveToLocal = (objName, obj) => {
+    AsyncStorage.setItem(objName, JSON.stringify(obj))
+    .then(console.log(AsyncStorage.getItem('inventory')))
+  }
+
+  getLocalStorage = async () => {
+
+    try {
+      let inventoryString = await AsyncStorage.getItem('inventory');
+      let inventory = JSON.parse(inventoryString);
+      let buyListString = await AsyncStorage.getItem('buyList');
+      let buyList = JSON.parse(inventoryString);
+
+      if (inventoryString) {
+        this.setState({
+          inventory: JSON.parse(inventoryString)
+        })
+      }
+
+      if (buyListString) {
+        this.setState({
+          buyList: JSON.parse(buyListString)
+        })
+      }
+
+    }
+    catch(error) {
+      console.log(error);
+    }
+  }
 
   addToBuyList = (key, item) => {
     const buyList = {...this.state.buyList}
     buyList[key] = item;
-    this.setState({ buyList });
+    this.setState({ buyList }, function() {
+      this.saveToLocal('buyList', this.state.buyList);
+    });  
   };
 
   addToInventory = (item) => {
     const inventory = {...this.state.inventory}
     inventory[item.name] = item;
-    this.setState({ inventory});
+    this.setState({ inventory }, function() {
+      this.saveToLocal('inventory', this.state.inventory);
+    }); 
   };
 
   render() {
