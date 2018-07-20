5b3466241c28ccdc56729280dcd00c8dd84099da
Jonathan Perez <jonathan.phz@gmail.com>
2018-07-20 15:25:23 -0300

    handle image and submit

--- src/screens/AddItem.js
+++ src/screens/AddItem.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import {Icon, Button, Input} from 'react-native-elements';
+import {Icon, Button, Input, Tile} from 'react-native-elements';
 
 
 export default class AddItem extends React.Component {
+
+  state = {
+    name: null,
+    image: null,
+  }
  
   goToCamera = () => {
-    this.props.navigation.navigate('Camera')
+    this.props.navigation.navigate('Camera', {handleImage: this.handleImage});
   };
 
   handleSubmit = () => {
     //Create item object
     const item = {
       name: this.state.name,
+      image: this.state.image
     }
     //Send item object to addToInventory methond on app then it adds it to state
     this.props.screenProps.addToInventory(item)
-  }
+  };
+
+  handleImage = (image) => {
+    this.setState({ image });
+    console.log(image);
+  };
 
   
   render() {
@@ -25,9 +36,17 @@ export default class AddItem extends React.Component {
       <View style={styles.itemContainer}>
         
         <TouchableOpacity onPress={() => this.goToCamera()}>
+        {!this.state.image &&
           <View style={styles.imageTile}>
             <Icon name="add-a-photo" size={35} type="material-icons" />
           </View>
+        }
+        {this.state.image &&
+          <Tile
+            imageSrc={{ uri: this.state.image }}
+            featured
+          />
+        }
         </TouchableOpacity>
 
         <View style={styles.formContainer}>
@@ -36,6 +55,7 @@ export default class AddItem extends React.Component {
             label="Product Name" 
             onChangeText={(name) => this.setState({ name })}
             />
+  
           <View style={styles.barcodeContainer}>
             <Icon name="barcode-scan" type="material-community" size={55} />
             <Text style={styles.fakeLable}>Barcode</Text>
